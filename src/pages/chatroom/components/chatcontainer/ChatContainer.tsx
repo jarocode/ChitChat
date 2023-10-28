059972c09fda7d9218e6e4082984e5126c08a722
jarocode <okolieuchenna890@gmail.com>
2023-10-28 18:57:57 +0100

    chat

--- src/pages/chatroom/components/chatcontainer/ChatContainer.tsx
+++ src/pages/chatroom/components/chatcontainer/ChatContainer.tsx
@@ -1,77 +1,82 @@
-
 import React from 'react';
-import styled from "styled-components";
+import styled from 'styled-components';
 import { Avatar, Typography } from '@mui/material';
 
-import {color }from "theme";
+import { color } from 'theme';
 
 interface ChatContainerProps {
-    name : string;
-    isUser: boolean;
-    time: string;
-    message: string;
+  name: string;
+  isUser: boolean;
+  time: string;
+  message: string;
 }
 
-const ChatContainer: React.FC<ChatContainerProps> = ({ name, isUser, time, message }) => {
-    return (
-        <Container isUser={isUser}>
-            <Div isUser={isUser}> 
-               <AvatarDiv isUser={isUser}>
-               <Avatar sx={{bgColor: "#f00"}}>{name.charAt(0)}</Avatar>
-                <Typography fontSize={"12px"}>{time}</Typography>
-               </AvatarDiv>
-                <ChatBox isUser={isUser}><Typography fontSize={"12px"}>{message}</Typography></ChatBox>
-            </Div>
-        </Container>
-    );
+const ChatContainer: React.FC<ChatContainerProps> = ({
+  name,
+  isUser,
+  time,
+  message,
+}) => {
+  return (
+    <Container isUser={isUser}>
+      <Div isUser={isUser}>
+        <AvatarDiv isUser={isUser}>
+          <Avatar sx={{ bgColor: '#f00' }}>{name.charAt(0)}</Avatar>
+          <Typography fontSize={'12px'}>{time}</Typography>
+        </AvatarDiv>
+        <ChatBox isUser={isUser}>
+          <Typography fontSize={'12px'}>{message}</Typography>
+        </ChatBox>
+      </Div>
+    </Container>
+  );
 };
 
 export default ChatContainer;
 
 interface ContainerProps {
-    isUser?: boolean
+  isUser?: boolean;
 }
 
 interface ChatBoxProps {
-    isUser?: boolean
+  isUser?: boolean;
 }
 
 interface DivProps {
-    isUser?: boolean
+  isUser?: boolean;
 }
 interface AvatarDivProps {
-    isUser?: boolean
+  isUser?: boolean;
 }
 
-
 const Container = styled.div<ContainerProps>`
- width: 100%;
- margin-bottom: 1rem;
- display: flex;
- justify-content: ${props => props.isUser ? "flex-end" : "flex-start"} ;
+  width: 100%;
+  margin-bottom: 1rem;
+  display: flex;
+  justify-content: ${(props) => (props.isUser ? 'flex-end' : 'flex-start')};
 `;
 
 const Div = styled.div<DivProps>`
- display: flex;
- justify-content: ${props => props.isUser ? "flex-end" : "flex-start"} ;
+  display: flex;
+  justify-content: ${(props) => (props.isUser ? 'flex-end' : 'flex-start')};
   width: 20rem;
-  gap: .7rem;
+  gap: 0.7rem;
   min-height: 3rem;
 `;
 
 const AvatarDiv = styled.div<AvatarDivProps>`
-   width: 3.5rem;
-   order:${props => props.isUser ? 2 : 1};
-  
+  width: 3.5rem;
+  order: ${(props) => (props.isUser ? 2 : 1)};
 `;
 
 const ChatBox = styled.div<ChatBoxProps>`
-  background: ${props => props.isUser ? color.brand2 : color.brand1};
+  background: ${(props) => (props.isUser ? color.brand2 : color.brand1)};
   color: ${(props) => (props.isUser ? color.brand1 : color.white)};
   order: ${(props) => (props.isUser ? 1 : 2)};
   min-height: 1rem;
   min-width: 5rem;
   max-width: 15rem;
- padding: 1rem;
-  border-radius:  ${(props) => (props.isUser ? "15px 0px 15px 15px" : "0px 15px 15px 15px")};
+  padding: 1rem;
+  border-radius: ${(props) =>
+    props.isUser ? '15px 0px 15px 15px' : '0px 15px 15px 15px'};
 `;
